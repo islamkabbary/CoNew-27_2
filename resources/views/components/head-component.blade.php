4ba1bb95cf8e21f11c6a4e87f6c8ec52fccbb775
islam kabbary <islamkabbary@gmail.com>
2024-02-27 15:11:33 +0200

    npm font aw

--- resources/views/components/head-component.blade.php
+++ resources/views/components/head-component.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ isset($seo_title) ? $seo_title->value() : $title }}</title>
-    <meta name="description" content="{{ $description ?? '' }}">
-    <link rel="canonical" href="{{ $canonical_link ?? url()->current() }}">
+    {{-- <title>{{ isset($seo_title) ? $seo_title->value() : $title }}</title> --}}
+    {{-- <meta name="description" content="{{ $description ?? '' }}"> --}}
+    {{-- <link rel="canonical" href="{{ $canonical_link ?? url()->current() }}"> --}}
     {{-- {!! html_entity_decode($meta_tags ?? '') !!} --}}
-    <link rel="shortcut icon" href="{{ getImage('images/icon-logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/icon-logo.png') }}" type="image/x-icon">
     {{-- <meta name="google-site-verification" content="R1LOVjSh5mEqZNI4_GdoUu8TcidGZjrA_KJUfZ_-GGY" /> --}}
     {{-- <meta name="facebook-domain-verification" content="fieo0ueleucvr4hnl8l11r8lghqdap" /> --}}
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -17,5 +17,11 @@
     {{-- <script src="https://checkout.tabby.ai/tabby-promo.js"></script> --}}
     {{-- {!! $custom_tag_seo->custom_head ?? '' !!} --}}
     @livewireStyles
-    {!! $script_header['script'] !!}
+    {{-- {!! $script_header['script'] !!} --}}
 </head>
+
+<body>
+
+</body>
+
+</html>
